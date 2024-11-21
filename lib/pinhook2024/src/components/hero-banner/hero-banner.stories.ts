4be4c07e86a2716f55e6dbb403e997b79e74e7e1
Stuart Grant <stu@campgrantnc.com>
2024-11-20 23:46:23 -0500

    Getting HeroCarousel to render, imports for styles in shadow DOM

--- lib/pinhook2024/src/components/hero-banner/hero-banner.stories.ts
+++ lib/pinhook2024/src/components/hero-banner/hero-banner.stories.ts
@@ -1,9 +1,6 @@
 import { html } from "lit";
 import type { Meta } from "@storybook/web-components";
 
-import { Default as Header } from "../../modules/header/header.stories";
-import { Default as ServiceCards } from "../service-card/service-card.stories";
-
 const meta: Meta = {
   title: "Hero Banner",
   component: "px-hero-banner",
@@ -16,35 +13,23 @@ export default meta;
 
 export const Default = () => {
   return html`
-    ${Header()}
-
-    <div class="bg-black text-white" style="margin-top:5rem;">
-      <px-hero-banner overlay>
-
-        <img src="https://picsum.photos/id/153/1920/1080" slot="background" />
-
-        <tui-content slot="content" alignment="start" gap="md">
-          <div slot="eyebrow">
-            <a href="/" class="text-inherit">Pinhook Excavation</a>
-            <span>/</span>
-            <span class="fw-bold" href="#">Our Services</span>
-          </div>
-
-          <h1 slot="title" class="fw-bold">Lorem Ipsum Dolor Sit Amet</h1>
-          <div slot="description" class="text-md">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-              convallis libero in dui mattis, nec ultricies nunc fermentum.
-            </p>
-          </div>
-          <a class="button" slot="cta">Contact Us</a>
-        </tui-content>
-
-      </px-hero-banner>
-    </div>
-
-    <div style="margin-top:-6rem;padding-bottom:6rem;">
-      ${ServiceCards()}
-    </div>
+    <px-hero-banner overlay>
+      <img src="https://picsum.photos/id/153/1920/1080" slot="background" />
+      <tui-content slot="content" alignment="start" gap="md">
+        <div slot="eyebrow">
+          <a href="/" class="text-inherit">Pinhook Excavation</a>
+          <span>/</span>
+          <span class="fw-bold" href="#">Our Services</span>
+        </div>
+        <h1 slot="title" class="fw-bold">Lorem Ipsum Dolor Sit Amet</h1>
+        <div slot="description" class="text-md">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            convallis libero in dui mattis, nec ultricies nunc fermentum.
+          </p>
+        </div>
+        <a class="button" slot="cta">Contact Us</a>
+      </tui-content>
+    </px-hero-banner>
   `;
 };
--- lib/pinhook2024/src/modules/hero-carousel/hero-carousel.stories.ts
+++ lib/pinhook2024/src/modules/hero-carousel/hero-carousel.stories.ts
@@ -1,13 +1,23 @@
 import { html } from "lit";
 import type { Meta } from "@storybook/web-components";
 
+import "./hero-carousel.js";
+
+import { Default as HeroBanner } from "../../components/hero-banner/hero-banner.stories";
+
 const meta: Meta = {
   title: "Hero Carousel",
   component: "px-hero-carousel",
+  parameters: {
+    layout: "fullscreen",
+  },
 };
 
 export default meta;
 
 export const Default = () => {
-  return html`<px-hero-carousel></px-hero-carousel>`;
+  return html`
+  <px-hero-carousel>
+    ${Array.from({ length: 4 }).map(() => HeroBanner())}
+  </px-hero-carousel>`;
 }
