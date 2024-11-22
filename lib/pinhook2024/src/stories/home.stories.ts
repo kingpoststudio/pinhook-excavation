03c034c4e4b67c80d7ff5e88ed4d44b634cc4e57
Stuart Grant <stu@campgrantnc.com>
2024-11-22 12:05:12 -0500

    Build with sibling components in mind

--- lib/pinhook2024/src/stories/home.stories.ts
+++ lib/pinhook2024/src/stories/home.stories.ts
@@ -1,9 +1,10 @@
 import { html } from 'lit';
 
-import { Default as Header } from "../modules/header/header.stories.js";
-// import { Default as Footer } from "../modules/footer/footer.stories.js";
-import { Default as HeroBanner } from "../components/hero-banner/hero-banner.stories.js";
-// import { ServiceCards } from "../components/img-card/img-card.stories.js";
+import { Default as Header } from '../modules/header/header.stories.js';
+// import { Default as Footer } from '../modules/footer/footer.stories.js';
+import { Default as HeroBanner } from '../components/hero-banner/hero-banner.stories.js';
+import { Default as ServiceCards } from '../components/service-card/service-card.stories.js';
+import { Default as RelatedArticles } from '../modules/card-carousel/card-carousel.stories.js';
 
 export default {
   title: 'Page: Home',
@@ -17,5 +18,11 @@ export const Default = () => html`
 
   <main>
     ${HeroBanner()}
+    <div style="margin-top:-6rem;">
+      ${ServiceCards()}
+    </div>
+    <div style="padding-block:4rem;">
+      ${RelatedArticles()}
+    </div>
   </main>
 `;
