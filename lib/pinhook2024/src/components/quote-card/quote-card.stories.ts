c1730326972165b8e19b02a3201dae5988308f48
Stuart Grant <stu@campgrantnc.com>
2024-11-22 16:06:00 -0500

    Make it work the background settings

--- lib/pinhook2024/src/components/quote-card/quote-card.stories.ts
+++ lib/pinhook2024/src/components/quote-card/quote-card.stories.ts
@@ -13,12 +13,14 @@ export default meta;
 
 export const Default = () => {
   return html`
+  <div class="bg-orange">
     <px-quote-card>
       <px-icon icon="quiltOne" slot="media"></px-icon>
       <p class="text-lg" slot="quote">
         Pinhook Excavation handled the site preparation for our new home with precision and care. They managed every detail, from clearing the land to grading the foundation, ensuring everything was ready on schedule. Their professionalism and expertise gave us confidence throughout the entire process.
       </p>
-      <p slot="author" class="text-lg">Bob Happy</p>
+      <p slot="author" class="text-lg">Bob Happy, <br /><strong>Graham County Homeowner</strong></p>
     </px-quote-card>
+  </div>
   `;
 };
