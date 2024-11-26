d7089478da56ef85aabc72dcd92a1659266243fa
Stuart Grant <stu@campgrantnc.com>
2024-11-25 20:41:23 -0500

    Some more updates to Hero

--- libs/pinhook2024/src/components/hero-banner/hero-banner.stories.ts
+++ libs/pinhook2024/src/components/hero-banner/hero-banner.stories.ts
@@ -19,11 +19,11 @@ export const Default = () => {
         <div slot="eyebrow">
           <span class="fw-bold" href="#">Pinhook Excavation</span>
         </div>
-        <h1 slot="title" class="fw-bold">Groundwork for a Rock-Solid Future.</h1>
+        <h1 slot="title" class="fw-bold">Groundwork for a Rock-Solid Future</h1>
         <div slot="description" class="text-md">
-          <p>Pinhook Excavation specializes in professional site preparation, land clearing, grading, and foundation excavation tailored to the unique demands of the Appalachian region. With a focus on precision, safety, and efficiency, we deliver dependable solutions for residential, commercial, and industrial projects. Our expertise ensures every project starts on solid ground.</p>
+          <p>Specializing in site preparation, foundational work, gravel roads, and more, we deliver professional results with a commitment to integrity and precision.</p>
         </div>
-        <a class="button" slot="cta">Our Services</a>
+        <a slot="cta" class="button" href="/services">Our Services</a>
       </tui-content>
     </px-hero-banner>
   `;
