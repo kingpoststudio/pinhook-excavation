51b496a16316b906ab2b36020aabc21a10e019a0
Stuart Grant <stu@campgrantnc.com>
2024-12-06 12:22:48 -0500

    Some bits and pieces

--- libs/pinhook2024/src/components/index.ts
+++ libs/pinhook2024/src/components/index.ts
@@ -1,6 +1,7 @@
 import "@kingpoststudio/truss-ui/dist/container.js";
 import "@kingpoststudio/truss-ui/dist/content.js";
 import "@kingpoststudio/truss-ui/dist/dialog.js";
+import "@kingpoststudio/truss-ui/dist/tab-group.js";
 import "@kingpoststudio/truss-ui/dist/tooltip.js";
 
 import "./animate/animate.js";
